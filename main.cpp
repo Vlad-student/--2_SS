270a3719edff376503965f6a6bf3a7d53b9085a5
Dmytro Cheremisov <cheremisovdmitiy@gmail.com>
2026-06-04 11:47:12 +0300

    Add function matricesAreEqual

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+const int max_size_of_matrix = 10;
+
+bool matricesAreEqual(int matrix1[][max_size_of_matrix], int matrix2[][max_size_of_matrix], int size) {
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            if (matrix1[i][j] != matrix2[i][j])
+                return false;
+    return true;
 }
