0789314cdd6ce7f2ae1e0e26515051ab7ca955b9
Vlad <vlad.v.terletskyi>
2026-06-04 10:59:52 +0300

    Add function loadFromFile

--- main.cpp
+++ main.cpp
@@ -15,5 +15,42 @@ void printMatrix(int matrix[][max_size_of_matrix], int size) {
     cout << endl;
 }
 
+bool loadFromFile(int matrix[][max_size_of_matrix], int size) {
+    ifstream file("colors.txt");
+    if (!file.is_open()) return false;
+
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            matrix[i][j] = 0;
+
+    int row = 0, col = 0;
+    char ch;
+    int currentNumber = 0;
+    bool haveNumber = false;
+
+    while (file.get(ch) && row < size) {
+        if (ch >= '0' && ch <= '9') {
+            currentNumber = currentNumber * 10 + (ch - '0');
+            haveNumber = true;
+        }
+        else if (ch == ' ' || ch == '\t') {
+            if (haveNumber) {
+                matrix[row][col] = currentNumber;
+                col++;
+                currentNumber = 0;
+                haveNumber = false;
+            }
+        }
+        else if (ch == '\n') {
+            if (haveNumber) {
+                matrix[row][col] = currentNumber;
+                currentNumber = 0;
+                haveNumber = false;
+            }
+            row++;
+            col = 0;
+            if (row >= size) break;
+        }
+    }
 
 int main(){}
