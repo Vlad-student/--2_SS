56f18253d3fa39d63ad1a285523a3988faf80790
Vlad <vlad.v.terletskyi>
2026-06-04 10:59:20 +0300

    Add function printMatrix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+const int max_size_of_matrix = 10;
+
+void printMatrix(int matrix[][max_size_of_matrix], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
+
+
+int main(){}
